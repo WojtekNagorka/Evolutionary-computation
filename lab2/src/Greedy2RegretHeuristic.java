3e057a8bebda7a965de97c0a82ed70daf72f2ecb
KubaCzech <kuba.aleksander.czech@gmail.com>
2025-10-17 20:07:11 +0200

    helper classes needed for lab2

---
+++ lab2/src/Greedy2RegretHeuristic.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Greedy2RegretHeuristic extends TSPSolver{
+    public Greedy2RegretHeuristic(double[][] distanceMatrix, List<Node> nodes){
+        super(distanceMatrix, nodes);
+    }
+
+    public void solve(){ //change void to Result
+        //
+    }
+}
---
+++ lab2/src/GreedyHeuristicWithWeightedSumCriterion.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class GreedyHeuristicWithWeightedSumCriterion extends TSPSolver{
+    public GreedyHeuristicWithWeightedSumCriterion(double[][] distanceMatrix, List<Node> nodes){
+        super(distanceMatrix, nodes);
+    }
+
+    public void solve(){ // change void to Result
+        //
+    }
+}
